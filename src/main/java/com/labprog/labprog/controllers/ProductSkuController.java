acabcf6ca39630c7d910fe10e79e63bf79e32cd3
MarkiNzera <vstar516@gmail.com>
2025-01-19 17:24:24 -0300

    feat: product sku service and controller (incomplete)

---
+++ src/main/java/com/labprog/labprog/controllers/ProductSkuController.java
@@ -0,0 +1,43 @@
+package com.labprog.labprog.controllers;
+
+import com.labprog.labprog.model.entities.ProductSkus;
+import com.labprog.labprog.services.ProductSkuServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/productSku")
+public class ProductSkuController {
+    @Autowired
+    private ProductSkuServices productSkuServices;
+
+    @PostMapping
+    public ProductSkus createProductSku(@RequestBody ProductSkus productSku) {
+        return productSkuServices.createProductSku(productSku);
+    }
+
+    @GetMapping
+    public List<ProductSkus> getAllProductSkus() {
+        return productSkuServices.getAllProductSkus();
+    }
+
+    @GetMapping("/{id}")
+    public ProductSkus getProductSkuById(@PathVariable UUID id) {
+        return productSkuServices.getProductSkuById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ProductSkus updateProductSku(@PathVariable UUID id, @RequestBody ProductSkus productSku) {
+        return productSkuServices.updateProductSku(id, productSku);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteProductSkuById(@PathVariable UUID id) {
+        productSkuServices.deleteProductSkuById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/labprog/labprog/services/ProductSkuServices.java
+++ src/main/java/com/labprog/labprog/services/ProductSkuServices.java
@@ -1,4 +1,47 @@
 package com.labprog.labprog.services;
 
+import com.labprog.labprog.model.entities.ProductSkus;
+import com.labprog.labprog.model.entities.Products;
+import com.labprog.labprog.model.repositories.ProductSkusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class ProductSkuServices {
+
+    @Autowired
+    private ProductSkusRepository productSkusRepository;
+
+    public ProductSkus createProductSku(ProductSkus productSku) {
+        return productSkusRepository.save(productSku);
+    }
+
+    public List<ProductSkus> getAllProductSkus() {
+        return productSkusRepository.findAll();
+    }
+
+    public ProductSkus getProductSkuById(UUID id) {
+        return productSkusRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product sku not found with id" + id));
+    }
+
+    public ProductSkus updateProductSku(UUID id, ProductSkus productSkuData) {
+
+        ProductSkus productSku = getProductSkuById(id);
+
+        productSku.setProduct(productSkuData.getProduct());
+        productSku.setPrice(productSkuData.getPrice());
+        productSku.setQuantity(productSkuData.getQuantity());
+
+        return productSkusRepository.save(productSku);
+    }
+
+    public void deleteProductSkuById(UUID id) {
+        ProductSkus productSku = getProductSkuById(id);
+        productSkusRepository.delete(productSku);
+    }
+
 }
