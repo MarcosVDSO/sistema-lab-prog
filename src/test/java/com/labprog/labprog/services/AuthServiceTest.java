b48ee6e46f25fbbbbae925878649908b964c70d3
MarkiNzera <vstar516@gmail.com>
2025-03-16 19:38:46 -0300

    test: authService test

---
+++ src/test/java/com/labprog/labprog/services/AuthServiceTest.java
@@ -0,0 +1,57 @@
+package com.labprog.labprog.services;
+
+import com.labprog.labprog.model.entities.Addresses;
+import com.labprog.labprog.model.entities.Users;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class AuthServiceTest {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    AuthService authService;
+
+    @Test
+    public void testIfUsernameExists() {
+
+        Users user = Users.builder()
+                .firstname("marcos")
+                .lastname("estrela")
+                .username("marcos.estrela")
+                .email("exemplo@email.com")
+                .cpf("00000000000")
+                .status("ENABLED")
+                .password("12345678")
+                .role("ADMIN")
+                .build();
+
+        Users savedUser = userService.save(user);
+        UserDetails foundedUser = authService.loadUserByUsername(user.getUsername());
+
+        Assertions.assertEquals(user.getUsername(), foundedUser.getUsername());
+
+    }
+
+    @Test
+    public void testIfUsernameNotExists() {
+        String username = "123123";
+
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> {
+            authService.loadUserByUsername(username);
+        });
+
+        Assertions.assertEquals("User not found with username: " + username, exception.getMessage());
+    }
+
+}
