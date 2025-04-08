f5c678803fe9a41bb1662e8b32a1f4e29b48759b
MarkiNzera <vstar516@gmail.com>
2025-04-08 00:26:13 -0300

    feat: updating cors policy

--- src/main/java/com/labprog/labprog/configs/CorsConfig.java
+++ src/main/java/com/labprog/labprog/configs/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:5173", "https://system-lab-prog-front-q1oo.vercel.app")
                         .allowedMethods("GET", "PUT", "DELETE", "POST")
                         .allowedHeaders("*")
                         .allowCredentials(true);
